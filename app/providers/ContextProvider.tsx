04b02a584b5574f434cd3daf79ab2178de95a4ac
abhinavanand-fed <abhinavanand080@gmail.com>
2024-04-01 00:01:08 +0530

    context provider

--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -1,14 +1,35 @@
 "use client";
-
 import React from "react";
-import GlobalStyleProvider from "./GlobalStyleProdvider";
+import { GlobalProvider } from "../context/globalProvider";
+import { Toaster } from "react-hot-toast";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function ContextProvider({ children }: Props) {
-  return <GlobalStyleProvider>{children}</GlobalStyleProvider>;
+  const [isReady, setIsReady] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 250);
+  }, []);
+
+  if (!isReady) {
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <span className="loader"></span>
+      </div>
+    );
+  }
+
+  return (
+    <GlobalProvider>
+      <Toaster />
+      {children}
+    </GlobalProvider>
+  );
 }
 
 export default ContextProvider;
